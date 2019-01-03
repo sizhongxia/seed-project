6a2d2f0b720f131f344e02c92ec012d0a158091c
sizhongxia <sizhongxia@haijupai.com>
2019-01-03 11:19:47 +0800

    修改获取天气

--- src/main/java/com/company/project/unit/HeFengWeather.java
+++ src/main/java/com/company/project/unit/HeFengWeather.java
@@ -4,6 +4,8 @@ import java.net.SocketTimeoutException;
 import java.util.Date;
 
 import org.apache.http.conn.ConnectTimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -11,20 +13,31 @@ import com.alibaba.fastjson.JSONObject;
 import com.company.project.model.SmartCultureWeatherNow;
 
 public class HeFengWeather {
-	private static final String KEY = "83ad6e357a724fc1b5d6900081dd6fbe";
+	static final Logger logger = LoggerFactory.getLogger(HeFengWeather.class);
+	// https://www.heweather.com/
+	// 1217491317@qq.com
+	private static final String[] KEYS = { "83ad6e357a724fc1b5d6900081dd6fbe", "a6417b1a1d674b3899ab6ad33910fba6",
+			"013e5bc12873403481186e64beff514a", "d734d55871a2474aa28b661773c1bdde",
+			"0ce45a2aa1954b74920c79ac1fc3ce09" };
 
 	public static SmartCultureWeatherNow queryNow(String cid) {
-		String jsonStr = reqWeatherInfo(cid);
-		if (jsonStr == null) {
-			return null;
-		}
-		return analysis(jsonStr);
+		SmartCultureWeatherNow wn = null;
+		int index = 0;
+		do {
+			String key = KEYS[index++];
+			logger.info("use key {} get weather info...", key);
+			wn = analysis(reqWeatherInfo(cid, key));
+			if (index >= KEYS.length) {
+				return null;
+			}
+		} while (wn == null);
+		return wn;
 	}
 
-	private static String reqWeatherInfo(String location) {
+	private static String reqWeatherInfo(String location, String key) {
 		try {
 			return HttpClientUtils.post("https://free-api.heweather.net/s6/weather/now",
-					String.format("location=%s&key=%s", location, KEY), "application/x-www-form-urlencoded", "UTF-8",
+					String.format("location=%s&key=%s", location, key), "application/x-www-form-urlencoded", "UTF-8",
 					6000, 6000);
 		} catch (ConnectTimeoutException e) {
 			e.printStackTrace();
@@ -41,25 +54,31 @@ public class HeFengWeather {
 		// "{\"HeWeather6\":[{\"basic\":{\"cid\":\"CN101010100\",\"location\":\"北京\",\"parent_city\":\"北京\",\"admin_area\":\"北京\",\"cnty\":\"中国\",\"lat\":\"39.90498734\",\"lon\":\"116.4052887\",\"tz\":\"+8.00\"},\"update\":{\"loc\":\"2018-12-26
 		// 20:56\",\"utc\":\"2018-12-26
 		// 12:56\"},\"status\":\"ok\",\"now\":{\"cloud\":\"0\",\"cond_code\":\"100\",\"cond_txt\":\"晴\",\"fl\":\"-11\",\"hum\":\"50\",\"pcpn\":\"0.0\",\"pres\":\"1036\",\"tmp\":\"-8\",\"vis\":\"11\",\"wind_deg\":\"237\",\"wind_dir\":\"西南风\",\"wind_sc\":\"1\",\"wind_spd\":\"3\"}}]}";
+		logger.info("req data: {}", jsonStr);
 		JSONObject jo = JSON.parseObject(jsonStr);
 		if (jo == null) {
+			logger.error("parse weather info error");
 			return null;
 		}
 		JSONArray heWeather6 = jo.getJSONArray("HeWeather6");
 		if (heWeather6 == null || heWeather6.size() < 1) {
+			logger.error("parse weather version error");
 			return null;
 		}
 		JSONObject weatherInfo = heWeather6.getJSONObject(0);
 		if (weatherInfo == null) {
+			logger.error("parse weather no data");
 			return null;
 		}
 		if (!"ok".equals(weatherInfo.getString("status"))) {
+			logger.error("parse weather is {}", weatherInfo.getString("status"));
 			return null;
 		}
 		JSONObject basic = weatherInfo.getJSONObject("basic");
 		JSONObject update = weatherInfo.getJSONObject("update");
 		JSONObject now = weatherInfo.getJSONObject("now");
 		if (basic == null || update == null || now == null) {
+			logger.error("parse weather format error");
 			return null;
 		}
 		SmartCultureWeatherNow wn = new SmartCultureWeatherNow();
--- src/main/java/com/company/project/web/basic/SmartCultureWeatherController.java
+++ src/main/java/com/company/project/web/basic/SmartCultureWeatherController.java
@@ -29,6 +29,8 @@ import com.company.project.service.SmartCultureWeatherHistoryService;
 import com.company.project.service.SmartCultureWeatherNowService;
 import com.company.project.unit.HeFengWeather;
 
+import cn.hutool.core.date.DateUtil;
+
 @RestController
 @RequestMapping("/basic/api/weather")
 public class SmartCultureWeatherController {
@@ -77,33 +79,58 @@ public class SmartCultureWeatherController {
 
 	@RequestMapping("/update")
 	public Result<?> update(HttpServletRequest request) {
+		logger.debug(">>>> Start Update Weather Info...");
 		List<SmartCultureWeatherCity> wcs = smartCultureWeatherCityService.findAll();
 		if (wcs == null) {
 			return ResultGenerator.genFailResult("E5001");
 		}
 		int num = 0;
 		for (SmartCultureWeatherCity cid : wcs) {
+			logger.debug(">>>>{}, Checking {} Weather", num, cid.getCityCode());
 			SmartCultureWeatherNow wn = smartCultureWeatherNowService.findBy("basicCid", cid.getCityCode());
-			if (wn != null && System.currentTimeMillis() - wn.getUpdateAt().getTime() < ((2 * 60 * 60 - 1) * 1000)) {
+			if (wn != null && System.currentTimeMillis()
+					- DateUtil.parse(wn.getUpdateLoc(), "yyyy-MM-dd HH:mm").getTime() < (180 * 60 * 1000)) {
+				logger.debug(">>>>{}, {} Weather No Update.", num, cid.getCityCode());
 				continue;
 			}
+			logger.debug(">>>>{}, Loading {} Weather Info...", num, cid.getCityCode());
 			SmartCultureWeatherNow nwn = HeFengWeather.queryNow(cid.getCityCode());
+			if (nwn == null) {
+				logger.debug(">>>>{}, Try Loading {} Weather Info...", num, cid.getCityCode());
+				try {
+					Thread.sleep(100L);
+				} catch (InterruptedException e) {
+				}
+				nwn = HeFengWeather.queryNow(cid.getCityCode());
+				if (nwn == null) {
+					logger.error(">>>>{}, Loading {} Weather Error.", num, cid.getCityCode());
+					continue;
+				}
+			}
 			if (wn == null) {
+				logger.debug(">>>>{}, Instert {} Weather Info.", num, nwn.getBasicCid());
 				smartCultureWeatherNowService.save(nwn);
 			} else {
 				long id = wn.getId();
 				if (nwn.getUpdateLoc().equals(wn.getUpdateLoc())) {
+					logger.debug(">>>>{}, Don`t Update {} Weather Info.", num, nwn.getBasicCid());
 					continue;
 				}
+				logger.debug(">>>>{}, Update {} Weather Info.", num, nwn.getBasicCid());
 				BeanUtils.copyProperties(nwn, wn);
 				wn.setId(id);
 				smartCultureWeatherNowService.update(wn);
 			}
+			logger.debug(">>>>{}, Instert {} History Weather Info.", num, nwn.getBasicCid());
 			SmartCultureWeatherHistory wh = new SmartCultureWeatherHistory();
 			BeanUtils.copyProperties(nwn, wh);
 			wh.setCreateAt(new Date());
 			smartCultureWeatherHistoryService.save(wh);
 			num++;
+			try {
+				Thread.sleep(10L);
+			} catch (InterruptedException e) {
+			}
 		}
 		return ResultGenerator.genSuccessResult(num);
 	}
